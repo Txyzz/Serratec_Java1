f39eff023b87eece7d9fb2700acbcf1cfcd23509
Txyzz <65177591+Txyzz@users.noreply.github.com>
2020-08-13 14:01:57 -0300

    Obra de Arte

---
+++ telaSaida.java
@@ -0,0 +1,112 @@
+package appInternetBanking;
+
+public class telaSaida {
+	public static void obraDeArte() throws InterruptedException {
+		
+        
+System.out.println("                          `-://::.`.ymmddhysoo++ooshhdddhs:  .oyhhhys+:`");                   
+Thread.sleep(120);  
+System.out.println("                      ./ymNNMMMMMNNNMMMMMMMMMMMMMMMMMMMMMMMhomMMMMMMMMMNy-");                        
+Thread.sleep(120);  
+System.out.println("                    :yNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN-");                       
+Thread.sleep(120);  
+System.out.println("                  `sNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM+");                      
+Thread.sleep(120);  
+System.out.println("                  +MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMo---.`");                  
+Thread.sleep(120); 
+System.out.println("                `-yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNNNNmds:`");              
+Thread.sleep(120);  
+System.out.println("             `/ymNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNm/");             
+Thread.sleep(120); 
+System.out.println("            :dMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN+");            
+Thread.sleep(120);  
+System.out.println("           /NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN`");           
+Thread.sleep(120);  
+System.out.println("          `dMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM-");          
+Thread.sleep(120);  
+System.out.println("          .NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNmmmmmmmNMMMMMMMMMMMMMMMMMMMMMM.");           
+Thread.sleep(120);  
+System.out.println("          /MMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNMMMMMMMMNho:-......:+hMMMMMMMMMMMMMMMMMMMMMd`");          
+Thread.sleep(120);  
+System.out.println("         `hMMMMMMMMMMMMMMMMMMMMMMMMMms/----/sdNMMd:        `hmMMMMMMMMMMMMMMMMMMMMMMM/");           
+Thread.sleep(120);  
+System.out.println("         sMMMMMMMMMMMMMMMMMMMMMMMMm/``.......:dMd.         `NMMMMMMMMMMMMMMMMMMMMMMMy`");          
+Thread.sleep(120);  
+System.out.println("        .NMMMMMMMMMMMMMMMMMMMMMMMM:  :ssyyyyyys+.           hMMMNNMMMMMMMMMMMMMMMMMMmdhyo-`");      
+Thread.sleep(120);  
+System.out.println("        `hMMMMMMMMMMMMMMMMMMMMMMMM.                         :hy+:hMMMMMMMMMMMMMMMMMMMMMMMNy`");      
+Thread.sleep(120);  
+System.out.println("         .yNMMMMMMMMMMMMMMMMMMMMMM.                            `yMMMMMMMMMMMMMMMMMMMMMMMMMMs");     
+Thread.sleep(120);  
+System.out.println("           -ohmMMMMMMMMMMMMMMMMMMM.                            /MMMMMMMMMMMMMMMMMMMMMMMMMMMh");     
+Thread.sleep(120);  
+System.out.println("         -sso++sMMMMMMMMMMMMMMMMMh                     `....```+MMMMMMMMMMMMMMMMMMMMMMMMMMMy");     
+Thread.sleep(120);  
+System.out.println("         yMMMMMMMMMMMMMMMMMNNNMNs.````              .+hmNNNNNmddMMMMMMMMMMMMMMMMMMMMMMMMMMN:");     
+Thread.sleep(120);  
+System.out.println("         yMMMMMMMMMMMMMMMd/--::-/yhhhys+.          /mMNNdyooooosydNNMMMMMMMMMMMMMMMMMMMMMh-");      
+Thread.sleep(120);  
+System.out.println("         +MMMMMMMMMMMMMMN.      `:mMy.`::         :NNy/o+mNNh/os+-`-/yNMMMMMMMMMMMMMMMMN+");         
+Thread.sleep(120);  
+System.out.println("          sMMMMMMMMMMMMMM:   `/yhdNNddhs/`       `dm: ```+yy/  `-:    `oNMMMMMMMMMMMMMMo");         
+Thread.sleep(120);  
+System.out.println("           +dNMMMMMMMMMMMs  -hMMMMMMMMMMMms/ys+/:+N-.:oyhdhhyo:       -.oMMMMMMMMMMMMN+");        
+Thread.sleep(120);  
+System.out.println("             .:+oyNMMMMMMm`+NMMMMMMMMMMMMMN:hs::/+dNMMMMMMMMMMMh`    om--MMMMMMMMMMMMm-");          
+Thread.sleep(120); 
+System.out.println("              `+hNMMMMMMMM:`sMMMMMMMMMMMMMm.h`    dMMMMMMMMMMMMMy` .hh. .NMMMMMMMMMMMMMy.");       
+Thread.sleep(120);  
+System.out.println("              sMMMMMMMMMMMh  +NMMMMMMMMMMd:``     +MMMMMMMMMMMMMMmhd/   :MMMMMMMMMMMMMMMN/");      
+Thread.sleep(120);  
+System.out.println("              oMMMMMMMMMMMN-  .+yhddhys/.          hMMMMMMMMMMMN+``     sMMMMMMMMMMMMMMMMm`");      
+Thread.sleep(120);  
+System.out.println("              `/ymMMMMMMMMMs                       `dmydmmmmmdy-       `mMMMMMMMMMMMMMMMMm`");      
+Thread.sleep(120);  
+System.out.println("                 -NMMMMMMMMh                        /m````````         +MMMMMdMMMMMMMMMMM+");       
+Thread.sleep(120);  
+System.out.println("                 -MMMMMMMMMd        `.-/+++:`    .:`sh`               -NMMMMM-+MMMMMMMMm/");        
+Thread.sleep(120);  
+System.out.println("                 `+mMMMMMMMd     `/ymNNMMMMNdo/:/sNh+-`              .dMMMMMM. +NMMMNd+.");        
+Thread.sleep(120);  
+System.out.println("                   `/ymNMMMo   .omMMMMMMMMMMMMMMMMMMMNmho:`         .dMMMMMMN.  .:+/.`");         
+Thread.sleep(120);  
+System.out.println("                      `.:/:` `omMMMMMMMMMMMMMMMMMMMMMMMMMNms:`     :mMMMMMMMh");                  
+Thread.sleep(120);  
+System.out.println("                             sMMMMMMMMmhhddmmmmmmdddMMMMMMMMNd+` .sNMMMMMMMd-");                  
+Thread.sleep(120);  
+System.out.println("                            `mMMMMMMMMms:.```...`.:hMMMMMMMMMMMhymMMMMMNmh+.");                    
+Thread.sleep(120);  
+System.out.println("                           -hMMMMMMMMMMMNmy+/:/+sdNMMMMMMMMMMMMMMm+++/-.``");                   
+Thread.sleep(120);  
+System.out.println("                          /mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNs.");                            
+Thread.sleep(120);  
+System.out.println("                         .NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMd-");                           
+Thread.sleep(120);  
+System.out.println("                         /MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMd`");                          
+Thread.sleep(120);  
+System.out.println("                         `omMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM-");                         
+Thread.sleep(120);  
+System.out.println("                           `:oydMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNMMMms`");                          
+Thread.sleep(120);  
+System.out.println("                               -MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMd://:.");                           
+Thread.sleep(120);  
+System.out.println("                               /MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMd.");                             
+Thread.sleep(120);  
+System.out.println("                               +MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm.");                              
+Thread.sleep(120);  
+System.out.println("                               -NMMMMMMMMMMMMMMMMMMMMMMMMMMMMNh-");                               
+Thread.sleep(120);  
+System.out.println("                                /dNMMMMMMMMMMMMMMMMMMMMMMNNmh:");                                  
+Thread.sleep(120);  
+System.out.println("                                  .:+odMMMMMMMMMMMMMMMMNo:-`");                                    
+Thread.sleep(120);  
+System.out.println("                                      `yNMMMMMMMMMMMNNy-");                                        
+Thread.sleep(120);  
+System.out.println("                                        -+ydmmmdhyo+:`");                                         
+Thread.sleep(120);  
+
+		
+		
+	}
+
+}
